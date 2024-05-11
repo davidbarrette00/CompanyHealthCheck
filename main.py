42af15a1c57d89ce64a6da7ca5d8af7e0594e82e
David Barrette <david.barrette00@gmail.com>
2024-05-11 14:41:25 -0700

    add functions for getting data from alphavantage

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import anthropic
+
+from data import alpha_vantage_service
+
+#https://docs.anthropic.com/claude/docs/quickstart-guide
+
+davids_key = "sk-ant-api03-XPu...DAAA"
+govindas_key = ""
+sindhus_key = ""
+jirens_key = ""
+
+# client = anthropic.Anthropic(
+#     api_key = davids_key #REPLACE WITH YOUR KEY
+# )
+#
+# message = client.messages.create(
+#     model="claude-3-opus-20240229",
+#     max_tokens=1000,
+#     temperature=0.0,
+#     system="Respond only in Yoda-speak.",
+#     messages=[
+#         {"role": "user", "content": "How are you today?"}
+#     ]
+# )
+#
+# print(message.content)
+
+print(alpha_vantage_service.get_overview("TSLA"))
